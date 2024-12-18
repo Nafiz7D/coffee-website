6d465c8602e8736b75cf7dce86faf44ff2015e70
unknown <mrnafsu07@gmail.com>
2024-12-18 20:46:52 +0600

    added whytobuy file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero/Hero'
 import Services from './components/Services/Services';
+import WhereToBuy from './components/WhereToBuy/WhereToBuy';
 
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
       <Hero></Hero>
       <Services></Services>
+      <WhereToBuy></WhereToBuy>
     </div>
   );
 };
